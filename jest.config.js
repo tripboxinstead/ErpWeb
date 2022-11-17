06c5e2d1afa6f40c421c6854220ccb4b18ac9ee7
instead <hackerjy@kakao.com>
2022-11-18 00:48:57 +0900

    tt

--- jest.config.js
+++
@@ -1,5 +0,0 @@
-moduleNameMapper : {
-    '^react-dnd$': 'react-dnd/dist/cjs',
-    '^react-dnd-html5-backend$': 'react-dnd-html5-backend/dist/cjs',
-    '^dnd-core$': 'dnd-core/dist/cjs',
-}
--- src/api.js
+++ src/api.js
@@ -1,4 +1,5 @@
 import axios from 'axios';
+// import axios from './lib/axios.js';
 
 const api = axios.create({
     baseURL : "https://supplyapi.tripbox.co.kr",
