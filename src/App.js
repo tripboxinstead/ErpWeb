84305151b2341e183db4f906ab603fca67baa4e2
instead <hackerjy@kakao.com>
2022-11-18 01:25:49 +0900

    router 수정

--- src/App.js
+++ src/App.js
@@ -1,20 +1,21 @@
 
 import './App.css';
 import './css/common.css';
-import { Route, Routes } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import TourDetail from './pages/TourDetail';
 import NotFound from './pages/NotFound';
 
 function App() {
   return (
     
-    <div className="App">
-      <Routes>
-       
+    <div className="App">    
+      <Router>
+      <Routes>       
       <Route path="/:product_id" element={<TourDetail/>} />
         <Route path="/Detail/:product_id" element={<TourDetail/>} />
         <Route path="/*" element={<NotFound />} />
       </Routes>
+      </Router>  
      
     </div>
     
